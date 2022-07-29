55d54cfc44347373877737b0f408676eceed1482
AliSaidSaritemur <80647702+AliSaidSaritemur@users.noreply.github.com>
2022-07-29 15:14:04 +0300

    Sensible alert

--- log4jClockDemo/src/log4jClockDemo/LoggingDemoClock.java
+++ log4jClockDemo/src/log4jClockDemo/LoggingDemoClock.java
@@ -16,15 +16,14 @@ public class LoggingDemoClock {
 		
 	
 		 
-		// TODO Auto-generated method stub
+		
 		logger.getLevel();
 while (true) {
 	
 	if(logger.getLevel().toString()=="ERROR") {
 		
 		  DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH");  
-		   LocalDateTime now = LocalDateTime.now();  
-		   System.out.println(dtf.format(now)+":00:00");  
+		   LocalDateTime now = LocalDateTime.now();    
 		logger.error(dtf.format(now)+":00:00");
 		Thread.sleep(3600000);
 	}
@@ -32,7 +31,6 @@ while (true) {
 	
 		 DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");  
 		   LocalDateTime now = LocalDateTime.now();  
-		   System.out.println(dtf.format(now)+":00"); 
 		   logger.info(dtf.format(now)+":00");
 			Thread.sleep(6000);
 		
@@ -41,7 +39,6 @@ else if(logger.getLevel().toString()=="DEBUG") {
 	
 	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm.ss");  
 	   LocalDateTime now = LocalDateTime.now();  
-	   System.out.println(dtf.format(now)); 
 	   logger.debug(dtf.format(now));
 	   Thread.sleep(1000);
 	   
